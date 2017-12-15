d3ccdd35ab0129f174704be2076bd066dca6f3e7
Simon Gog <simon.gog@gmail.com>
2017-12-15 09:05:03 +0100

    Adjsuted licence information

--- include/sdsl/hyb_sd_vector.hpp
+++ include/sdsl/hyb_sd_vector.hpp
@@ -1,19 +1,6 @@
-/* sdsl - succinct data structures library
-    Copyright (C) 2016 Simon Gog, Matthias Petri
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see http://www.gnu.org/licenses/ .
-*/
+// Copyright (c) 2016, the SDSL Project Authors.  All rights reserved.
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
 /*!\file sd_vector.hpp
    \brief sd_vector.hpp contains the sdsl::sd_vector class, and
           classes which support rank and select for sd_vector.
