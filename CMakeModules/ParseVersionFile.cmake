7f5cfc030e2614d5676f2818a9880da5e3b8cf82
cpockrandt <noreply-github@cpockrandt.de>
2018-02-12 15:30:06 +0100

    version.hpp added

--- CMakeModules/ParseVersionFile.cmake
+++ CMakeModules/ParseVersionFile.cmake
@@ -1,7 +1,16 @@
-## parse the VERSION file in the sdsl directory 
+## parse the VERSION file in the sdsl directory
+set (VERSION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/include/sdsl/version.hpp")
 
-file(READ "${CMAKE_CURRENT_SOURCE_DIR}/VERSION" LIBRARY_VERSION_FULL)
-string(REGEX REPLACE "[\n\r]" "" LIBRARY_VERSION_FULL "${LIBRARY_VERSION_FULL}")
-string(REGEX REPLACE "^([0-9]+)\\.[0-9]+\\.[0-9]+$" "\\1" LIBRARY_VERSION_MAJOR "${LIBRARY_VERSION_FULL}")
-string(REGEX REPLACE "^[0-9]+\\.([0-9]+)\\.[0-9]+$" "\\1" LIBRARY_VERSION_MINOR "${LIBRARY_VERSION_FULL}")
-string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+)$" "\\1" LIBRARY_VERSION_PATCH "${LIBRARY_VERSION_FULL}")
+if (NOT EXISTS ${VERSION_FILE})
+  message ("")
+  message ("ERROR: Could not determine SDSL version.")
+  message ("Could not find file: ${VERSION_FILE}")
+else ()
+    file (READ ${VERSION_FILE} LIBRARY_VERSION_CODE)
+    string (REGEX MATCH "define SDSL_VERSION_MAJOR ([0-9]+)" _ ${LIBRARY_VERSION_CODE})
+    set(LIBRARY_VERSION_MAJOR ${CMAKE_MATCH_1})
+    string (REGEX MATCH "define SDSL_VERSION_MINOR ([0-9]+)" _ ${LIBRARY_VERSION_CODE})
+    set(LIBRARY_VERSION_MINOR ${CMAKE_MATCH_1})
+    string (REGEX MATCH "define SDSL_VERSION_PATCH ([0-9]+)" _ ${LIBRARY_VERSION_CODE})
+    set(LIBRARY_VERSION_PATCH ${CMAKE_MATCH_1})
+endif ()
