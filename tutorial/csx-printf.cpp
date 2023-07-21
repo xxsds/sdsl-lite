18d5e3aecf5abaf2ad71cde6af5d9fd771f220bb
Enrico Seiler <enrico.seiler@hotmail.de>
2023-07-21 16:15:32 +0200

    [FIX] ambiguous call
    Clash with std::format

--- tutorial/csx-printf.cpp
+++ tutorial/csx-printf.cpp
@@ -6,7 +6,6 @@
 #include <sdsl/cst_sct3.hpp>
 
 using namespace sdsl;
-using namespace std;
 
 std::string format("%3I%3S %3s %3P %3p %3L %3B  %T");
 std::string header("  i SA ISA PSI  LF LCP BWT  TEXT");
@@ -19,19 +18,19 @@ typedef cst_sct3<csa_int_t> cst_int_t;
 
 void print_usage(char const * command)
 {
-    cout << "\
+    std::cout << "\
 A pretty printer for suffix array/tree members.\n\
 Transforms each input line into a CST and outputs\n\
 formatted suffix array/tree members.\n\
 Usage: " << command
-         << " X \"[FORMAT]\" \"[HEADER]\" \"[SENTINEL]\"\n\
+              << " X \"[FORMAT]\" \"[HEADER]\" \"[SENTINEL]\"\n\
 X       : Input is interpreted dependent on X.\n\
          X=1: byte sequence.\n\
          X=d: sequence of decimal numbers.\n\
 FORMAT  : Format string. Default=`"
-         << format << "`.\n\
+              << format << "`.\n\
 HEADER  : Header string. Default=`"
-         << header << "`.\n\
+              << header << "`.\n\
 SENTINEL: Sentinel character. \n\
 \n\
 Each line of the output will be formatted according to the format string.\
@@ -68,24 +67,24 @@ int main(int argc, char * argv[])
     {
         header = argv[3];
     }
-    while (cin.getline(line, BUF_SIZE))
+    while (std::cin.getline(line, BUF_SIZE))
     {
-        cout << header << endl;
+        std::cout << header << std::endl;
         if ('1' == argv[1][0])
         {
             cst_byte_t cst;
             construct_im(cst, (char const *)line, 1);
-            stringstream ss;
+            std::stringstream ss;
             csXprintf(ss, format, cst, ((argc > 4) ? argv[4][0] : '$'));
             std::string line(ss.str());
-            cout << std::regex_replace(line, std::regex(R"(\$)"), "\\$") << endl;
+            std::cout << std::regex_replace(line, std::regex(R"(\$)"), "\\$") << std::endl;
         }
         else if ('d' == argv[1][0])
         {
             cst_int_t cst;
             construct_im(cst, (char const *)line, 'd');
-            csXprintf(cout, format, cst, ((argc > 4) ? argv[4][0] : '0'));
+            csXprintf(std::cout, format, cst, ((argc > 4) ? argv[4][0] : '0'));
         }
-        cout << endl;
+        std::cout << std::endl;
     }
 }
